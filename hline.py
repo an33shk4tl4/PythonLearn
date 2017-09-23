a090725fe5b4227b9a3127007299930e31f4dc1e
bluef1sh2015 <aneeshuvce@gmail.com>
2017-09-23 06:02:26 -0500

    generator function and custome exceptions

---
+++ hline.py
@@ -0,0 +1,15 @@
+def h(char='-', repeat=80):
+    """
+    function to print repeat characters
+    very handy to draw a horizontal line for logging purpose
+    usage : h('*') default repeat = 80
+    """
+    import random
+    sq = list('abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+[{]};:'"")
+    # print char[0]*repeat
+    print random.choice(sq)*repeat
+
+h()
+
+# h('_')
+#TODO create a function to spill out random characters to make a hline
